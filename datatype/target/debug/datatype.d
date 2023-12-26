497925271a738ceb63e477b3281ce986c3ab3cab
daiyadeguchi <double.d.rugby@gmail.com>
2023-12-26 11:49:55 +0900

    data type weeee

---
+++ datatype/target/debug/datatype.d
@@ -0,0 +1,1 @@
+/Users/daiyadeguchi/dev/rust/datatype/target/debug/datatype: /Users/daiyadeguchi/dev/rust/datatype/src/main.rs
---
+++ datatype/target/debug/deps/datatype-c81a8bf0f05d8898.d
@@ -0,0 +1,5 @@
+/Users/daiyadeguchi/dev/rust/datatype/target/debug/deps/datatype-c81a8bf0f05d8898: src/main.rs
+
+/Users/daiyadeguchi/dev/rust/datatype/target/debug/deps/datatype-c81a8bf0f05d8898.d: src/main.rs
+
+src/main.rs:
